df7e588a75a6fa85c1005368dfc77bf22ce2c439
Nizar Windows <nize1963@gmail.com>
2023-03-09 15:32:32 +0300

    first commit

---
+++ test2.py
@@ -0,0 +1,14 @@
+# echo "# github" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/nize1963/github.git
+# git push -u origin main
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/nize1963/github.git
+# git branch -M main
+# git push -u origin main
+# 1
+print("Nizar2")
+print("Abbod")
